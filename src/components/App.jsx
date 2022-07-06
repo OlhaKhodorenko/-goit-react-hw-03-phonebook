c36c1c55a67d6fade526b881a7a591df7989cb10
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-07-06 23:54:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,10 @@ export default class App extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
-    this.setState({ contacts: parsedContacts });
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
   changeFilter = e => {
